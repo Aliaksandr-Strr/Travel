a2adb1575498e27e9d505f5705c2d8abc8cb339d
1481169@mail.ru <1481169@mail.ru>
2020-05-28 13:32:09 +0300

    Закончена работа с приложением поездов

--- trains/views.py
+++ trains/views.py
@@ -1,8 +1,8 @@
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import TrainForm
@@ -22,4 +22,28 @@ class TrainCreatView(SuccessMessageMixin, CreateView):
     form_class = TrainForm
     template_name = 'trains/create.html'
     success_url = reverse_lazy('train:home')
-    success_message = 'Город успешно создан'
+    success_message = 'Поезд успешно создан'
+
+
+class TrainDetailView(DetailView):
+    queryset = Train.objects.all()
+    context_object_name = 'object'
+    template_name = 'trains/detail.html'
+
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('train:home')
+    success_message = 'Поезд успешно отредактирован'
+
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    # template_name = 'cities/delete.html'
+    success_url = reverse_lazy('train:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Поезд успешно удален!')
+        return self.post(request, *args, **kwargs)
